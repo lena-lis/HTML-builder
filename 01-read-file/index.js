1451bc5453101658d68732dc12f23a4a4b2fdc23
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2024-01-21 11:49:16 +0300

    feat: solve 1st task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
+  encoding: 'utf8',
+});
+readStream.on('data', (dataChunk) => {
+  console.log(dataChunk);
+});
