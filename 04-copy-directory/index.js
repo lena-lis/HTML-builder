8952a49b6953fa0d5425d228a18d70565bc6602c
Elena Lisichenok <111569149+lena-lis@users.noreply.github.com>
2024-01-23 21:02:37 +0300

    fix & refactor: improve bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,38 +1,23 @@
-const { mkdir, readdir, copyFile, unlink } = require('fs/promises');
+const { rm, mkdir, readdir, copyFile } = require('fs/promises');
 const path = require('path');
 
 const srcPath = path.join(__dirname, 'files');
 const destPath = path.join(__dirname, 'files-copy');
 
-async function deleteFiles() {
-  const files = await readdir(destPath);
-  for (const file of files) {
-    const filePath = path.join(destPath, file);
-    unlink(filePath);
-  }
-}
-
-async function makeDirectory() {
-  try {
-    const destFolder = path.join(__dirname, 'files-copy');
-    const dirCreation = await mkdir(destFolder, {
-      recursive: true,
-    });
-    return dirCreation;
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-async function copyFiles(from, to) {
-  const srcFolder = await readdir(from, { withFileTypes: true });
-  for (const file of srcFolder) {
-    if (file.isFile()) {
-      await copyFile(from + '//' + file.name, to + '//' + file.name);
+rm(destPath, { recursive: true, force: true })
+  .then(() => {
+    return mkdir(destPath, { recursive: true });
+  })
+  .then(() => {
+    return readdir(srcPath, { withFileTypes: true });
+  })
+  .then((files) => {
+    for (const file of files) {
+      if (file.isFile()) {
+        copyFile(srcPath + '//' + file.name, destPath + '//' + file.name);
+      }
     }
-  }
-}
-
-deleteFiles();
-makeDirectory();
-copyFiles(srcPath, destPath);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
